075d5a92df80806746f99f250b080fe2aab00a15
Dominik <domi77fullhd@gmail.com>
2020-12-21 18:25:52 +0100

    Crude implementation of TCPserver + code cleanup

--- lib/menu.cpp
+++ lib/menu.cpp
@@ -1,5 +1,7 @@
 #include "menu.h"
 
+    int key_temp = 0;
+
     Menu::Menu()
     {
         //set correct settings for ncurses input 
@@ -18,57 +20,55 @@
         init_pair(2, COLOR_WHITE, COLOR_BLACK); //Unselected color pair
     }
 
-
-    void _redraw_periodically()
+    void start_server()
     {
-        
+        /** \brief create TCPserver object*/
+        TCPserver _srv(5555, 25);
+        /** \brief start the server on a new thread so that it runs concurrently*/
+        thread server(&TCPserver::run, &_srv);
+        server.detach();
     }
 
     void Menu::create_menu()
     {
-        //get size of terminal width and height and store it in _winsize...
-        ioctl(STDOUT_FILENO, TIOCGWINSZ, &_winsize);
-
         //create key variable to store pressed key in
         int key = 1;
-
-        //draw menu for the first time
-        draw_menu();
 
         while (_menuActive)
         {
             // flushinp(); //flush ncurses input buffer before getting input
             key = getch();
+            key == -1 ? 1 : key_temp = key;
             switch (key)
             {
-                case 259:
+                case arrow_up:
                     switch (_currentScreen)
                     {
-                        case home:
+                        case homeScreen:
                             switch (_selectionScreen)
                             {
-                                case selectClient:
-                                    _selectionScreen = goExit;
+                                case startClientItem:
+                                    _selectionScreen = exitItem;
                                     break;
-                                case selectServer:
-                                    _selectionScreen = selectClient;
+                                case startServerItem:
+                                    _selectionScreen = startClientItem;
                                     break;
-                                case goExit:
-                                    _selectionScreen = selectServer;
+                                case exitItem:
+                                    _selectionScreen = startServerItem;
                                     break;
                             }
-                        case client:
+                        case clientScreen:
                             switch (_clientScreen)
                             {
-                                case joinGame:
-                                    _clientScreen = back;
+                                case joinGameItem:
+                                    _clientScreen = backItem;
                                     break;
-                                case back:
-                                    _clientScreen = joinGame;
+                                case backItem:
+                                    _clientScreen = joinGameItem;
                                     break;
                             }
                             break;
-                        case server:
+                        case serverScreen:
                             switch (_serverScreen)
                             {
                                 case startServer:
@@ -84,34 +84,34 @@
                             break;
                     }
                     break;
-                case 258:
+                case arrow_down:
                     switch (_currentScreen)
                     {
-                        case home:
+                        case homeScreen:
                             switch (_selectionScreen)
                             {
-                                case selectClient:
-                                    _selectionScreen = selectServer;
+                                case startClientItem:
+                                    _selectionScreen = startServerItem;
                                     break;
-                                case selectServer:
-                                    _selectionScreen = goExit;
+                                case startServerItem:
+                                    _selectionScreen = exitItem;
                                     break;
-                                case goExit:
-                                    _selectionScreen = selectClient;
+                                case exitItem:
+                                    _selectionScreen = startClientItem;
                                     break;
                             }
-                        case client:
+                        case clientScreen:
                             switch (_clientScreen)
                             {
-                                case joinGame:
-                                    _clientScreen = back;
+                                case joinGameItem:
+                                    _clientScreen = backItem;
                                     break;
-                                case back:
-                                    _clientScreen = joinGame;
+                                case backItem:
+                                    _clientScreen = joinGameItem;
                                     break;
                             }
                             break;
-                        case server:
+                        case serverScreen:
                             switch (_serverScreen)
                             {
                                 case startServer:
@@ -127,11 +127,28 @@
                             break;
                     }
                     break;
-                case 10:
-                    switch (_selectionScreen)
+                case enter:
+                    switch (_currentScreen)
                     {
-                        case goExit:
-                            exit(1);
+                        case homeScreen:
+                            switch (_selectionScreen)
+                            {
+                                case startServerItem:
+                                    _currentScreen = serverScreen;
+                                    start_server();
+                                    break;
+                                case exitItem:
+                                    exit(1);
+                                    break;
+                            }
+                            break;
+                    }
+                    break;
+                case key_q:
+                    switch (_currentScreen)
+                    {
+                        case serverScreen:
+                            //stop the server and go back to homeScreen, still needs implementation...
                             break;
                     }
                     break;
@@ -145,26 +162,34 @@
         //get size of terminal width and height and store it in _winsize...
         ioctl(STDOUT_FILENO, TIOCGWINSZ, &_winsize);
 
+        //clear the screen
         clear();
+
+        //draw width, height and pressed button id information in the upper left corner
+        attron(COLOR_PAIR(2)); //set text output color pair, which are defined further up...
+        printw("key_id: %d\nx: %d\ny: %d", key_temp, _winsize.ws_col, _winsize.ws_row);
+
+        //drawing screen
         switch (_currentScreen)
         {
-            case home:
-                attron(COLOR_PAIR(2));
-                printw("x: %d y: %d\n", _winsize.ws_col, _winsize.ws_row);
-                _selectionScreen == selectClient ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
+            case homeScreen:
+                attron(COLOR_PAIR(2)); //set text output color pair, which are defined further up...
+                _selectionScreen == startClientItem ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
                 move(_winsize.ws_row/2-2, _winsize.ws_col/2-6);
                 printw("Start Client\n");
-                _selectionScreen == selectServer ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
+                _selectionScreen == startServerItem ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
                 move(_winsize.ws_row/2-1, _winsize.ws_col/2-6);
                 printw("Start Server\n");
-                _selectionScreen == goExit ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
+                _selectionScreen == exitItem ? attron(COLOR_PAIR(1)) : attron(COLOR_PAIR(2));
                 move(_winsize.ws_row/2, _winsize.ws_col/2-2);
                 printw("Exit\n");
                 break;
-            case server:
-                /* code */
+            case serverScreen:
+                attron(COLOR_PAIR(2));
+                move(_winsize.ws_row/2-2, _winsize.ws_col/2-22);
+                printw("Server is running, to stop server press <Q>\n");
                 break;
-            case client:
+            case clientScreen:
                 /* code */
                 break;
         }
--- lib/menu.h
+++ lib/menu.h
@@ -3,24 +3,38 @@
 
 #define STDIN_FILENO 0
 
+#define arrow_up 259
+#define arrow_down 258
+#define enter 10
+#define key_q 113
+
 #include <iostream>
 #include "menu.h"
 #include <curses.h>
 #include <sys/ioctl.h> //ioctl() and TIOCGWINSZ
 #include <unistd.h> // for STDOUT_FILENO
+#include <thread>
+
+#include "simplesocket.h"
+#include <unistd.h> //contains various constants
+#include <sys/types.h> //contains a number of basic derived types that should be used whenever appropriate
+#include <arpa/inet.h> // defines in_addr structure
+#include <sys/socket.h> // for socket creation
+#include <netinet/in.h> //contains constants and structures needed for internet domain addresses
 
 using namespace std;
 
 /** \brief Enumerators to store the state of the menu in*/
-enum CurrentScreen {home, client, server};
-enum selectionScreen{selectClient, selectServer, goExit};
-enum clientScreen {joinGame, back};
-enum serverScreen {startServer, stopServer, goBack};
+enum CurrentScreen {homeScreen, clientScreen, serverScreen};
+enum SelectionScreen{startClientItem, startServerItem, exitItem};
+enum ClientScreen {joinGameItem, backItem};
+enum ServerScreen {startServer, stopServer, goBack};
 
 
 class Menu
 {
 private:
+
     /** \brief struct to store terminal size in*/
     struct winsize _winsize;
 
@@ -28,13 +42,12 @@ private:
     bool _menuActive = true;
 
     /** \brief Variables to store the state of the menu in*/
-    CurrentScreen _currentScreen = home;
-    selectionScreen _selectionScreen = selectClient;
-    clientScreen _clientScreen;
-    serverScreen _serverScreen;
+    CurrentScreen _currentScreen = homeScreen;
+    SelectionScreen _selectionScreen = startClientItem;
+    ClientScreen _clientScreen;
+    ServerScreen _serverScreen;
 
 public:
-
 
     /** \brief Menu constructor */
     Menu();
@@ -53,6 +66,7 @@ public:
 
     /** \brief Closes menu */
     void close_menu();
+
 };
 
 #endif /* MENU */
