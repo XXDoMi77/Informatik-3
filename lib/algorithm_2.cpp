497d5240b4e044aea23a76d47c0698d31567b5df
Freddy <meyer.fredric@t-online.de>
2021-01-10 19:00:39 +0100

    Implemented random for Algorithm_2 without checking double shots

--- lib/algorithm_2.cpp
+++ lib/algorithm_2.cpp
@@ -14,13 +14,14 @@ void Algorithm_2::run(TCPclient* _TCPclient)
     bool gameWon = false;
     char msg[25];
     string receivedMsg;
-    int tmpX = 1;
-    int tmpY = 1;
+    int tmpX;
+    int tmpY;
     while (!gameWon)
     {
         // tmpX = int(9/RAND_MAX*rand())+1;
         // tmpY = int(9/RAND_MAX*rand())+1;
-        
+        tmpX = rand() % 10 + 1;
+        tmpY = rand() % 10 + 1;
 
         sprintf(msg, "shoot %02d %02d", tmpX, tmpY);
 
@@ -47,22 +48,6 @@ void Algorithm_2::run(TCPclient* _TCPclient)
         else if(receivedMsg[0] == 'f')
         {
             gameWon = true;
-        }
-
-        if (tmpX <= 10)
-        {
-            tmpX++;
-        }
-        if (tmpX == 11)
-        {
-            tmpX = 1;
-        }
-        if (tmpY <= 10 && tmpX == 1)
-        {
-            if(tmpY != 10)
-            {
-                tmpY++;
-            }
         }
 
         usleep(100000);
